96ec8d864917a2491a56de61248c626fdde47033
ushac5517 <ushachoudhury5517@gmail.com>
2021-08-24 16:35:39 +1000

    Add new rest tests

--- src/test/java/RestTestSuite.java
+++ src/test/java/RestTestSuite.java
@@ -1,2 +1,19 @@
-package PACKAGE_NAME;public class RestTestSuite {
+import org.junit.jupiter.api.Test;
+
+import static io.restassured.RestAssured.*;
+import static org.hamcrest.Matchers.*;
+
+public class RestTestSuite {
+
+    @Test
+
+    public void Status200Test() {
+        given()
+                .param("q", "Tascott").and().header("auth-key", "76eb35d9-6e8b-4ac6-b0e7-a58334c5259e")
+                .when()
+                   .get("https://digitalapi.auspost.com.au/postcode/search.json")
+                .then()
+                   .assertThat().statusCode(200).and().body("localities.locality.postcode", equalTo(2250));
+    }
+
 }
