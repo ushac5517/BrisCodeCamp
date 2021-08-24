ea0e367a8b65170f720d9876ce3bcb7e1685e12b
ushac5517 <ushachoudhury5517@gmail.com>
2021-08-24 23:33:06 +1000

    Added GivenQuery_GetPostcodeSearch_PostcodeTest in RestTestSuite

--- src/test/java/RestTestSuite.java
+++ src/test/java/RestTestSuite.java
@@ -6,14 +6,23 @@ import static org.hamcrest.Matchers.*;
 public class RestTestSuite {
 
     @Test
-
     public void Status200Test() {
         given()
-                .param("q", "Tascott").and().header("auth-key", "76eb35d9-6e8b-4ac6-b0e7-a58334c5259e")
-                .when()
-                   .get("https://digitalapi.auspost.com.au/postcode/search.json")
-                .then()
-                   .assertThat().statusCode(200).and().body("localities.locality.postcode", equalTo(2250));
+                .param("q", "Tascott").and().header("auth-key", "76eb35d9-6e8b-4ac6-b0e7-a58334c5259e").
+        when()
+                .get("https://digitalapi.auspost.com.au/postcode/search.json").
+        then()
+                .assertThat().statusCode(200);
+    }
+
+    @Test
+    public void GivenQuery_GetPostcodeSearch_PostcodeTest() {
+        given()
+                .param("q", "Tascott").and().header("auth-key", "76eb35d9-6e8b-4ac6-b0e7-a58334c5259e").
+                when()
+                .get("https://digitalapi.auspost.com.au/postcode/search.json").
+                then()
+                .assertThat().statusCode(200).and().body("localities.locality.postcode", equalTo(2250));
     }
 
 }
